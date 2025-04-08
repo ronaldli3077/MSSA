8e47bdaf653fe5c8e4e6c306d25395402e4257ac
Ronald Li <ronaldli2009@gmail.com>
2025-04-08 09:28:42 -0700

    Add files via upload
    regular expression

---
+++ Week 3/3.2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+namespace Mod1RegularExpression
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            input:
+            Console.WriteLine("Enter your name");
+            string name=Console.ReadLine();
+            if(ContainsDigits(name))
+            {
+                Console.WriteLine("Enter only letters!");
+                goto input;
+            }
+            else
+            {
+                emailinput:
+                Console.WriteLine($"{name} , please enter your email id");
+
+                string email=Console.ReadLine();
+                if(CheckEmail(email))
+                {
+                    Console.WriteLine($"{name} , you are registered successfully!");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid email format, please reenter");
+                    goto emailinput;
+                }
+            }
+            Console.ReadKey();
+
+        }
+
+        static bool CheckEmail(string email)
+        {
+            string emailpattern = @"^[^@\s]+@[^@\s]+\.[^@\s]+$";
+            return Regex.IsMatch(email,emailpattern);
+        }
+        static bool ContainsDigits(string s)
+        {
+            return Regex.IsMatch(s, "\\d");
+            
+        }
+
+    }
+}
