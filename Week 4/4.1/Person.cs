fd700833eeaf230c3eae3bfae0447dcf725b1276
Ronald Li <ronaldli2009@gmail.com>
2025-04-16 23:31:48 -0700

    Add files via upload

---
+++ Week 4/4.1/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mod4GenericsDemo1
+{
+    internal class Person
+    {
+        public string FirstName {  get; set; }
+        public string LastName { get; set; }
+        public int WorkPhone {  get; set; }
+        public int HomePhone {  get; set; }
+    }
+}
---
+++ Week 4/4.1/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Mod4GenericsDemo1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * List
+             * Q
+             * Stack
+             * Dictionary
+             * SortedDictionary
+             * 
+             * 
+             */
+
+            Stack<int> intstack= new Stack<int>();
+            
+            Queue<string> customerQ = new Queue<string>();
+            customerQ.Enqueue("alex");
+
+            Dictionary<string,string> Recipes = new Dictionary<string,string>();
+            Recipes.Add("Clear Soup", "1.Boil broth..");
+            Recipes.Add("FRied Chicken", "1. Clean chciken..");
+
+            foreach(var key in Recipes.Keys)
+            {
+                Console.WriteLine(key + "-" + Recipes[key]);
+            }
+            Recipes.Remove("Clear Soup");
+
+
+            Dictionary<string,Person>phoneBook= new Dictionary<string,Person>();
+            if (phoneBook.ContainsKey("Deepali"))
+            {
+                Console.WriteLine("Person exists");
+            }
+            else
+            {
+                phoneBook.Add("Deepali", new Person() { FirstName = "Deepali", LastName = "K", HomePhone = 433434 });
+
+            }
+
+
+            
+        }
+    }
+}
